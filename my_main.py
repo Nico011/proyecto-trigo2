00bc9dc81f34a3cef50472a0c4bb31f5831da95d
Nicolás H <nicolas.hervias@live.com>
2021-04-08 17:04:02 -0400

    separación data preprocessing
    el pre procesamiento de datos se separa en su propio módulo

--- my_main.py
+++ my_main.py
@@ -19,7 +19,7 @@ def datos():
     datos = pandas.read_csv("data-total.csv", header=0 ,delimiter=";", encoding='ISO-8859-1')
     
     # Variable a predecir
-    target = "Chl"
+    # target = "Chl"
     
     # filtramos los datos con las siguientes condiciones
     # Año = 2016
@@ -27,146 +27,8 @@ def datos():
     # condición != secano
     # Genotipo = "QUP 2569-2009"
     
-    # filtro por año (2014)------------------------------------
-    filtro1_2014 = datos[datos["ANIO"] == 2014]
-    filtro2_2014 = datos[datos["ANIO"] == 2014]
+    return datos
     
-    filtro1_2014 = filtro1_2014[filtro1_2014["CONDICION"] != "SECANO"]
-    filtro2_2014 = filtro2_2014[filtro2_2014["CONDICION"] == "SECANO"]
-    
-    df_chl_control_2014 = filtro1_2014.loc[ : , target]
-    df_firma_control_2014 = filtro1_2014.loc[ : , "350":"2500"]
-    cols = list(df_firma_control_2014.columns.values) # recuperamos los nombres de columnas
-    
-    # Estandarizar control 2014
-    df_firma_control_2014 = pandas.DataFrame(StandardScaler().fit_transform(df_firma_control_2014)) 
-    df_firma_control_2014.columns = cols
-    
-    df_chl_secano_2014 = filtro2_2014.loc[ : , target]
-    df_firma_secano_2014 = filtro2_2014.loc[ : , "350":"2500"]
-    
-    # Estandarizar secano 2014
-    df_firma_secano_2014 = pandas.DataFrame(StandardScaler().fit_transform(df_firma_secano_2014)) 
-    df_firma_secano_2014.columns = cols
-    
-    # Unir columna a predecir con predictores
-    control_2014 = pandas.concat([df_chl_control_2014.reset_index(drop=True), df_firma_control_2014], axis = 1)
-    secano_2014 = pandas.concat([df_chl_secano_2014.reset_index(drop=True), df_firma_secano_2014], axis = 1)
-    
-    # eliminar NAs
-    control_2014.dropna(inplace = True)
-    secano_2014.dropna(inplace = True)
-    
-    # Reasignamos estas variables, pero ahora se eliminaron los NAs y se estandarizó
-    firma_control_2014 = control_2014.loc[ : , "350":"2500"]
-    firma_secano_2014 = secano_2014.loc[ : , "350":"2500"]
-    
-    
-    # filtro por año (2015) -----------------------------------
-    filtro1_2015 = datos[datos["ANIO"] == 2015]
-    filtro2_2015 = datos[datos["ANIO"] == 2015]
-    
-    filtro1_2015 = filtro1_2015[filtro1_2015["CONDICION"] != "SECANO"]
-    filtro2_2015 = filtro2_2015[filtro2_2015["CONDICION"] == "SECANO"]
-    
-    df_chl_control_2015 = filtro1_2015.loc[ : , target]
-    df_firma_control_2015 = filtro1_2015.loc[ : , "350":"2500"]
-    cols = list(df_firma_control_2015.columns.values) # recuperamos los nombres de columnas
-    
-    # Estandarizar control 2015
-    df_firma_control_2015 = pandas.DataFrame(StandardScaler().fit_transform(df_firma_control_2015)) 
-    df_firma_control_2015.columns = cols
-    
-    df_chl_secano_2015 = filtro2_2015.loc[ : , target]
-    df_firma_secano_2015 = filtro2_2015.loc[ : , "350":"2500"]
-    
-    # Estandarizar secano 2015
-    df_firma_secano_2015 = pandas.DataFrame(StandardScaler().fit_transform(df_firma_secano_2015)) 
-    df_firma_secano_2015.columns = cols
-    
-    # Unir columna a predecir con predictores
-    control_2015 = pandas.concat([df_chl_control_2015.reset_index(drop=True), df_firma_control_2015], axis = 1)
-    secano_2015 = pandas.concat([df_chl_secano_2015.reset_index(drop=True), df_firma_secano_2015], axis = 1)
-    
-    # eliminar NAs
-    control_2015.dropna(inplace = True)
-    secano_2015.dropna(inplace = True)
-    
-    # Reasignamos estas variables, pero ahora se eliminaron los NAs y se estandarizó
-    firma_control_2015 = control_2015.loc[ : , "350":"2500"]
-    firma_secano_2015 = secano_2015.loc[ : , "350":"2500"]
-    
-    
-    # filtro por año (2016) -----------------------------------------------
-    filtro1_2016 = datos[datos["ANIO"] == 2016]
-    filtro2_2016 = datos[datos["ANIO"] == 2016]
-    
-    filtro1_2016 = filtro1_2016[filtro1_2016["CONDICION"] != "SECANO"]
-    filtro2_2016 = filtro2_2016[filtro2_2016["CONDICION"] == "SECANO"]
-    
-    
-    df_chl_control_2016 = filtro1_2016.loc[ : , target]
-    df_firma_control_2016 = filtro1_2016.loc[ : , "350":"2499"]
-    cols = list(df_firma_control_2016.columns.values) # recuperamos los nombres de columnas
-    
-    # Estandarizar control 2016
-    df_firma_control_2016 = pandas.DataFrame(StandardScaler().fit_transform(df_firma_control_2016)) 
-    df_firma_control_2016.columns = cols
-    
-    df_chl_secano_2016 = filtro2_2016.loc[ : , target]
-    df_firma_secano_2016 = filtro2_2016.loc[ : , "350":"2499"]
-    
-    # Estandarizar secano 2016
-    df_firma_secano_2016 = pandas.DataFrame(StandardScaler().fit_transform(df_firma_secano_2016)) 
-    df_firma_secano_2016.columns = cols
-    
-    # Unir columna a predecir con predictores
-    control_2016 = pandas.concat([df_chl_control_2016.reset_index(drop=True), df_firma_control_2016], axis = 1)
-    secano_2016 = pandas.concat([df_chl_secano_2016.reset_index(drop=True), df_firma_secano_2016], axis = 1)
-    
-    # eliminar NAs
-    control_2016.dropna(inplace = True)
-    secano_2016.dropna(inplace = True)
-    
-    # Reasignamos estas variables, pero ahora se eliminaron los NAs y se estandarizó
-    firma_control_2016 = control_2016.loc[ : , "350":"2499"]
-    firma_secano_2016 = secano_2016.loc[ : , "350":"2499"]
-    
-    
-    # filtro por año 2017 -----------------------------------------------
-    filtro1_2017 = datos[datos["ANIO"] == 2017]
-    filtro2_2017 = datos[datos["ANIO"] == 2017]
-    
-    filtro1_2017 = filtro1_2017[filtro1_2017["CONDICION"] != "SECANO"]
-    filtro2_2017 = filtro2_2017[filtro2_2017["CONDICION"] == "SECANO"]
-    
-    df_chl_control_2017 = filtro1_2017.loc[ : , target]
-    df_firma_control_2017 = filtro1_2017.loc[ : , "350":"2500"]
-    cols = list(df_firma_control_2017.columns.values) # recuperamos los nombres de columnas
-    
-    # Estandarizar control 2017
-    df_firma_control_2017 = pandas.DataFrame(StandardScaler().fit_transform(df_firma_control_2017)) 
-    df_firma_control_2017.columns = cols
-    
-    df_chl_secano_2017 = filtro2_2017.loc[ : , target]
-    df_firma_secano_2017 = filtro2_2017.loc[ : , "350":"2500"]
-    
-    # Estandarizar secano 2017
-    df_firma_secano_2017 = pandas.DataFrame(StandardScaler().fit_transform(df_firma_secano_2017)) 
-    df_firma_secano_2017.columns = cols
-    
-    # Unir columna a predecir con predictores
-    control_2017 = pandas.concat([df_chl_control_2017.reset_index(drop=True), df_firma_control_2017], axis = 1)
-    secano_2017 = pandas.concat([df_chl_secano_2017.reset_index(drop=True), df_firma_secano_2017], axis = 1)
-    
-    # eliminar NAs
-    control_2017.dropna(inplace = True)
-    secano_2017.dropna(inplace = True)
-    
-    # Reasignamos estas variables, pero ahora se eliminaron los NAs y se estandarizó
-    firma_control_2017 = control_2017.loc[ : , "350":"2500"]
-    firma_secano_2017 = secano_2017.loc[ : , "350":"2500"]
-    return
 
 
 def rangos_clustering(data):
@@ -176,7 +38,7 @@ def rangos_clustering(data):
         return
     
     if len(data) == 1:
-        return data
+        return [(data, data)]
     
     ndata = [[d, d] for d in data]
     new_data = np.array(ndata)
@@ -226,13 +88,21 @@ def main(argv):
         elif opt in ("-t", "--target"):
             target = arg
         elif opt in ("-a", "--algorithm"):
-            alg = arg
+            alg = arg 
         elif opt in ("-y", "--year"):
             year = arg
+            
+    if year != 'all':
+        if alg == 'boruta':
+            
+            
+            
     
     print("Target:", target)
     print("Algoritmo:", alg)
     print("Anios:", year)    
+    
+    
 
 if __name__ == "__main__":
     main(sys.argv[1:])
